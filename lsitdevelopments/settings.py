e6ffc21f6c30a7cfe104321e2b96f4f6df58629d
lalitbharindwal <lalitbharindwal@gmail.com>
2024-11-18 20:26:39 +0530

    upload

--- lsitdevelopments/settings.py
+++ lsitdevelopments/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ql8h2wxq-^lqyhjet&e3fr$h2*$llk9a9pjj8b_k#2n5mzven1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
